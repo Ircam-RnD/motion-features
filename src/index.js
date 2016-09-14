562365cedd0e69a4ff19c89ed84e03583a7a9858
Joseph <joseph.larralde@gmail.com>
2016-09-14 18:48:56 +0200

    updated docs

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ const perfNow = getTimeFunction();
  * Class computing the descriptors from accelerometer and gyroscope data.
  * <br />
  * Example :
- * ```JavaScript.es6
+ * ```JavaScript
  * // es6 :
  * import MotionFeatures from 'motion-features'; 
  * const mf = new MotionFeatures({ descriptors: ['accIntensity', 'kick'] });
